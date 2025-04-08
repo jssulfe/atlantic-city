ca1d3dbd879c9c36215b74c1e97caf2deeb5ddfa
jesusUlfe <noeulfe@gmail.com>
2025-04-07 22:35:32 -0500

    ultimo update

---
+++ src/backend/reportes/reportes.js
@@ -0,0 +1,1 @@
+// van las funciones de logica de reportes, donde se detallara las reportes que se generan
---
+++ src/frontend/reportes/reportes.jsx
@@ -0,0 +1,1 @@
+// la vista de la pagina web de reportes, donde se detallara las reportes que se generan
